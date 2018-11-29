187aaee12c439de267c6c8270dc19b13c0d8e40c
Alexander Mutuc <AlexMutuc@Alexanders-MacBook-Pro.local>
2018-11-29 09:39:20 -0800

    Working form/div creation for job info

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+
+
     var config = {
         apiKey: "AIzaSyAakeEevKf-1RdnnBLDYpcOSZT9sVEoE8o",
         authDomain: "group-project-firebase.firebaseapp.com",
@@ -15,21 +17,23 @@ $(document).ready(function () {
     $("#post-btn").on("click", function (event) {
 
         event.preventDefault();
+        // Creates variables for every input field
         var titleInput = $("#title-input").val();
         var locationInput = $("#location-input").val();
         var timeInput = $("#time-input").val();
         var descriptionInput = $("#description-input").val();
         var linkInput = $("#link-input").val();
+        // Takes link input and prepends https:// if not included in input
+        var linkText = (linkInput.indexOf('://') === -1) ? 'http://' + linkInput : linkInput;
         var contactInput = $("#contact-input").val();
 
+        // pushes input data to firebase under job-listing folder
         database.ref("/job-listing").push({
             title: titleInput,
             location: locationInput,
             time: timeInput,
-            // part-time = 1
-            // full-time = 2
             description: descriptionInput,
-            link: linkInput,
+            link: linkText,
             contact: contactInput
         });
 
@@ -37,16 +41,42 @@ $(document).ready(function () {
 
     database.ref("/job-listing").on("child_added", function (childSnapshot) {
 
-        var jobBtn = ("<button>"+childSnapshot.val().title+"</button>");
+        // Creates a button everytime user submits a job labeled 
+        var jobBtn = $("<div>" + childSnapshot.val().title + "</div>");
         $("#job-links-display").append(jobBtn);
+        jobBtn.attr("id", "job-btn");
+        jobBtn.append("<p>"+childSnapshot.val().location+"</p>");
 
-        // $("#job-info-content").text(childSnapshot.val().title);
+        var modalTitle = $("<h1>" + childSnapshot.val().title + "</h1>");
+        var modalLocation = $("<p>" + childSnapshot.val().location + "</p>");
+        var modalTime = $("<p>" + childSnapshot.val().time + "</p>");
+        var modalDescription = $("<p>" + childSnapshot.val().description + "<p>");
+        var modalContact = $("<p>" + childSnapshot.val().contact+"</p>");
+        var modalLink = $(`<p><a href='${childSnapshot.val().link}'>For more info</a></p>`);
 
+        // When user clicks on job listing 
         jobBtn.on("click", function (event) {
             event.preventDefault();
-            console.log("Button Works");
+            var emptyModal = $("#job-info-modal");
+            emptyModal.modal('show');
+
+
+            var jobHeaderSpan = $(".modal-header-div");
+            var jobLocationSpan = $("#location-span")
+            var jobTypeSpan = $("#job-type-span");
+            var jobDescriptionSpan = $("#description-span");
+            var jobContactSpan = $("#contact-span");
+            var jobLinkSpan = $("#link-span");
+
+            jobHeaderSpan.html(modalTitle);
+            jobLocationSpan.html(modalLocation);
+            jobTypeSpan.html(modalTime);
+            jobDescriptionSpan.html(modalDescription);
+            jobContactSpan.html(modalContact);
+            jobLinkSpan.html(modalLink);
 
         });
+
 
     });
 
